496cc8e317ade31bba1567b7e9acb175efa822da
jakubklimek97 <jakukli853@student.polsl.pl>
2020-01-06 19:32:17 +0100

    added bootstrap

--- frontend/mistboard/src/app/app.module.ts
+++ frontend/mistboard/src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, MenuComponent]
 })
 export class AppModule { }
